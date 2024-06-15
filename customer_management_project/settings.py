51dc3efc52694b3e55c4aed319f1e97b59523514
popson <popsonemmanuel99@gmail.com>
2024-06-15 08:18:47 +0530

    first commit
    first commit

--- customer_management_project/settings.py
+++ customer_management_project/settings.py
@@ -71,15 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'customer_management_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'customer_management_db',
     }
 }
+
 
 
 # Password validation
